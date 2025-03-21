af4b02488acca5f52c8f4856b055185728c2e950
w-jins <jineen@naver.com>
2025-03-21 16:19:23 +0900

    gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="Netflix">
     <App />
   </BrowserRouter>
 );
